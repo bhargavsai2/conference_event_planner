2b4ce6b82ce8df8fca7758880089aeb5bf578944
Bhargav Sai <bhargavsai554@gmail.com>
2024-11-06 19:37:18 -0500

    updated venu, add-on, meals along handling state

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-
+import avReducer from './avSlice';
+import mealsSlice from './mealsSlice';
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
+    meals: mealsSlice,
   },
 });
